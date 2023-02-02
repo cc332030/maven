ffeed2581efce3f1d0b12c17b597fd11a5d4d38b
c332030 <c332030@gmail.com>
2023-02-02 15:44:51 +0800

    feat: maven init.bat

--- script/init.bat
+++ script/init.bat
@@ -2,15 +2,11 @@
 
 %utf8%
 
-::echo.
-::set /p maven=请选择 maven 目录路径：
 set maven=%~dp0data\wrapper\dists\apache-maven-3.6.3-bin\1iopthnavndlasol9gbrbg6bf2\apache-maven-3.6.3
 
-set app=data\app
+set app=%~dp0data\app
 
-echo.
-rd /s /q %app%
-mklink /h /j %app% %maven%
+call link-dir "%maven%" "%app%"
 
 set SETTING_NAME=settings-local.xml
 
@@ -21,6 +17,6 @@ call link-file %~dp0data\bin %app%\bin mvn8.bat
 
 call link-dir %~dp0data %userprofile%\.m2
 
-call link-dir F:\build\maven\repository %userprofile%\.m2\repository
+call link-dir F:\data\maven\repository %userprofile%\.m2\repository
 
 pause >nul
