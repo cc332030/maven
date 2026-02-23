2944e474b8568f6ace6fd424857ec78b381742b3
c332030 <c332030@c332030.com>
2026-02-23 11:00:27 +0800

    feat: init-maven.sh settings-env.xml

--- script/linux/init-maven.sh
+++ script/linux/init-maven.sh
@@ -12,77 +12,8 @@ mkdir -p "${WORK_PATH}"
 MAVEN_PROPERTIES=settings.xml
 MAVEN_PROPERTIES_PATH=${WORK_PATH}/${MAVEN_PROPERTIES}
 
-echo "
-<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-
-<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\"
-          xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-          xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd\" >
-
-    <servers>
-
-        <server>
-            <id>${NEXUS_SNAPSHOT_ID}</id>
-            <username>${NEXUS_USERNAME}</username>
-            <password>${NEXUS_PASSWORD}</password>
-        </server>
-
-        <server>
-            <id>${NEXUS_RELEASE_ID}</id>
-            <username>${NEXUS_USERNAME}</username>
-            <password>${NEXUS_PASSWORD}</password>
-        </server>
-
-    </servers>
-
-    <mirrors>
-        <mirror>
-            <id>central</id>
-            <url>${MAVEN_CENTRAL}</url>
-            <mirrorOf>central</mirrorOf>
-        </mirror>
-    </mirrors>
-
-    <profiles>
-        <profile>
-            <id>${NEXUS_ID}</id>
-
-            <activation>
-                <activeByDefault>true</activeByDefault>
-            </activation>
-
-            <repositories>
-
-                <repository>
-                    <id>${NEXUS_SNAPSHOT_ID}</id>
-                    <url>${NEXUS_SNAPSHOT_URL}</url>
-                    <snapshots>
-                        <enabled>true</enabled>
-                        <updatePolicy>always</updatePolicy>
-                    </snapshots>
-                    <releases>
-                        <enabled>false</enabled>
-                    </releases>
-                </repository>
-
-                <repository>
-                    <id>${NEXUS_RELEASE_ID}</id>
-                    <url>${NEXUS_RELEASE_URL}</url>
-                    <snapshots>
-                        <enabled>false</enabled>
-                    </snapshots>
-                    <releases>
-                        <enabled>true</enabled>
-                    </releases>
-                </repository>
-
-            </repositories>
-        </profile>
-    </profiles>
-
-</settings>
-
-" > "${MAVEN_PROPERTIES_PATH}"
+SETTING_URL=https://github.com/cc332030/maven/raw/refs/heads/master/conf/settings-env.xml
+curl -sL -o "${MAVEN_PROPERTIES_PATH} "${SETTING_URL}"
 
 USER=$(whoami)
 if [ "root" = "${USER}" ]
